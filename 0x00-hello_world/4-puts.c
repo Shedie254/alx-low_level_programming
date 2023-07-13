84be030ff39235cf22410a8a092f91cdd6da9492
Shedie254 <shadrackmuchiri43@gmail.com>
2023-07-13 08:13:25 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main -Entry point
  *
- * main :print message in the puts 
+ * Result :print message in the puts 
  * Return : Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
