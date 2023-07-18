f15d904160906a816bd2de71764a9c12d98cc267
Shedie254 <shadrackmuchiri43@gmail.com>
2023-07-18 00:03:19 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  * Description: it prints _putchar
+  * Return: 0 success
+  */
+int main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+return (0);
+}
