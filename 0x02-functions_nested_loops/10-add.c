9e90fd79c7d9d10809b0aed6f1fbdf9797b5d7f7
Shedie254 <shadrackmuchiri43@gmail.com>
2023-07-18 03:51:29 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * add: find sum of two numbers
+  * @i: first input number
+  * @j: second input number
+  * Return: results after addition
+  */
+int add(int i, int j);
+{
+	int sum = i + j;
+
+	return (sum);
+}
+
