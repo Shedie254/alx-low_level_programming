fb6edd9007325e2c2b35c983bd4f8560cf9f88c0
Shedie254 <shadrackmuchiri43@gmail.com>
2023-07-20 06:08:18 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,14 @@
 void print_line(int n)
 {
 	if (n <= 0)
-		_putchar('\n');
-	else
 	{
-	while (n > 0)
+		_putchar('\n');
+		return;
+	}
+	while (n < 0)
 	{
 		_putchar('_');
 		n--;
-	}
 	}
 	_putchar('\n');
 }
