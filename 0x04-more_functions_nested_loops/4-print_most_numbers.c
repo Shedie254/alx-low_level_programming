9cb4aa9097225a129a380ee93f1619a05df3ff78
Shedie254 <shadrackmuchiri43@gmail.com>
2023-07-20 05:35:03 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,7 +8,13 @@ void print_most_numbers(void)
 	int j;
 
 	for (j = 0; j >= 9; j++)
-		if (j != 2 || j != 4)
+	{
+		if (j == 2 || j == 4)
+		{
+			continue;
+			j++;
 			_putchar(j + '0');
+		}
 	_putchar('\n');
+	}
 }
