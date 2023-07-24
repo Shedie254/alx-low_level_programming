31ca1e1b699a0461a4a82c540681f9d459f48085
Shedie254 <shadrackmuchiri43@gmail.com>
2023-07-24 04:46:45 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+/**
+  * _puts - prints a string on std output
+  * @str: the string to be printed
+  */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
