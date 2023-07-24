7fa9433ca6bb7459a704298b283184eb65ae6780
Shedie254 <shadrackmuchiri43@gmail.com>
2023-07-24 04:33:53 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+/**
+  * _strlen - return lenth of a string
+  * @s: string to be examined
+  * Return: the lenth of a string
+  */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+	return (len);
+}
